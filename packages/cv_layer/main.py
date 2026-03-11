6fc35aa8276beb0c27ab5aef869fbce104762c8a
Yonas Gebremedhin <yonashailug@gmail.com>
2026-03-11 09:46:23 -0700

    init packages

---
+++ packages/cv_layer/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cv-layer!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/verifier/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from verifier!")
+
+
+if __name__ == "__main__":
+    main()
